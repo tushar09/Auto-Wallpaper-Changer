89e424419a3a94359798f71c952e5e7aaaba918a
Tushar <wtushar09@gmail.com>
2019-09-28 22:40:18 +0600

    api need to be changed. new key is inserted in base url

--- app/src/main/java/club/tushar/hdwallpaper/utils/Constants.java
+++ app/src/main/java/club/tushar/hdwallpaper/utils/Constants.java
@@ -11,15 +11,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Constants{
 
-    private static String BASE_URL = "https://api.unsplash.com/";
-    private static String CLIENT_ID = "8efd42a25f5bac80716327b6e95a80b2d5a900984dd453e9ce07715be7c7c159";
+    private static String BASE_URL = "https://pixabay.com/api/?key=13776425-388ae79a88daef057c1ae2c61";
 
-    public static String order_by = "popular";
     public static String orientation = "landscape";
 
     public static ApiServices getApiService(){
-//        OkHttpClient client = new OkHttpClient.Builder()
-//                .addInterceptor(new HeaderInterceptor(CLIENT_ID)).build();
 
         HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
         // set your desired log level
@@ -27,8 +23,7 @@ public class Constants{
         OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
         httpClient.connectTimeout(5, TimeUnit.MINUTES);
         httpClient.readTimeout(5, TimeUnit.MINUTES);
-        httpClient.addInterceptor(logging);  // <-- this is the important line!
-        httpClient.addInterceptor(new HeaderInterceptor(CLIENT_ID));  // <-- this is the important line!
+        httpClient.addInterceptor(logging);  // <-- this is the important line!  // <-- this is the important line!
 
         ApiServices retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
