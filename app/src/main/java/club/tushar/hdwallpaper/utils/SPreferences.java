3681494678e5e3f72308df23663a9dc45c9fd3d1
Tushar Monirul <monirul@misfit.tech>
2021-03-03 00:19:33 +0600

    setting ui done

--- app/src/main/java/club/tushar/hdwallpaper/utils/SPreferences.java
+++ app/src/main/java/club/tushar/hdwallpaper/utils/SPreferences.java
@@ -11,6 +11,10 @@ public class SPreferences {
 
     private String RESPONSE = "response";
 
+    private String ENABLE_SCREENLOCK_AUTO_CHANGE = "enableScreenlockAutoChange";
+    private String ENABLE_AUTO_CHANGE = "enableAutoChange";
+    private String TIMER_AUTO_CHANGE = "timerAutoChange";
+
     private SharedPreferences sp;
 
     public SPreferences(Context c) {
@@ -23,5 +27,29 @@ public class SPreferences {
 
     public PixelsResponse getResponse(){
         return new Gson().fromJson(sp.getString(RESPONSE, "na"), PixelsResponse.class);
+    }
+
+    public void setEnableScreenLockAutoChange(boolean isEnabled){
+        sp.edit().putBoolean(ENABLE_SCREENLOCK_AUTO_CHANGE, isEnabled).commit();
+    }
+
+    public boolean isEnabledScreenLockAutoChange(){
+        return sp.getBoolean(ENABLE_SCREENLOCK_AUTO_CHANGE, true);
+    }
+
+    public void setEnableAutoChange(boolean isEnabled){
+        sp.edit().putBoolean(ENABLE_AUTO_CHANGE, isEnabled).commit();
+    }
+
+    public boolean isEnabledAutoChange(){
+        return sp.getBoolean(ENABLE_AUTO_CHANGE, true);
+    }
+
+    public void setTimerAutoChange(int timer){
+        sp.edit().putInt(TIMER_AUTO_CHANGE, timer).commit();
+    }
+
+    public int getTimerAutoChange(){
+        return sp.getInt(TIMER_AUTO_CHANGE, 4);
     }
 }
